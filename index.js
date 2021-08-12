22e8462894ef6fc4eb8a4ea332d5c21ff7deada3
inexorableace <rik.saunders@icloud.com>
2021-08-12 09:38:55 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var $wrap = $('.wrapper'),
+  lFollowX = 5,
+  lFollowY = 10,
+  x = 0,
+  y = 0,
+  friction = 1 / 20;
+
+function animate() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  $wrap.css({
+    'transform': 'translate(0%, 0%) perspective(600px) rotateY(' + -x + 'deg) rotateX(' + y + 'deg)'
+  });
+  window.requestAnimationFrame(animate);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (12 * lMouseX) / 300;
+  lFollowY = (10 * lMouseY) / 300;
+
+});
+
+animate();
