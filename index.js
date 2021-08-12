c8e79c3b3baf4dc58cdb4055086f8039a24189e0
inexorableace <rik.saunders@icloud.com>
2021-08-12 10:32:06 -0400

    blur for safari?

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ $(window).on('mousemove click', function(e) {
 
   var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
   var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (12 * lMouseX) / 300;
+  lFollowX = (22 * lMouseX) / 300;
   lFollowY = (10 * lMouseY) / 300;
 
 });
